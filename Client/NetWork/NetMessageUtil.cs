0945a776efb4c305fe4dcbe93326c229cf1bc316
Khomskii <852927407@qq.com>
2017-03-23 01:02:41 +0800

    first commit

---
+++ Client/NetWork/NetMessageUtil.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameProtocol;
+
+public class NetMessageUtil : MonoBehaviour {
+
+    IHandler login;
+	// Use this for initialization
+	void Start () {
+        login = GetComponent<LoginHandler>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        while (NetIO.Instance.messages.Count > 0) {
+            SocketModel model= NetIO.Instance.messages[0];
+            StartCoroutine("MessageReceive", model);
+            NetIO.Instance.messages.RemoveAt(0);
+        }
+	    
+	}
+
+    void MessageReceive(SocketModel model) {
+        switch (model.type) { 
+            case Protocol.TYPE_LOGIN:
+                login.MessageReceive(model);
+                break;
+        }
+    }
+}
