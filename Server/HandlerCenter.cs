8eaa70f8b57253c71aa085014c7f8f7904758fc6
Khomskii <852927407@qq.com>
2017-03-23 01:05:46 +0800

    server

---
+++ Server/HandlerCenter.cs
@@ -0,0 +1,43 @@
+﻿using GameProtocol;
+using LOLServer.logic;
+using LOLServer.logic.login;
+using NetFrame;
+using NetFrame.auto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOLServer
+{
+   public class HandlerCenter:AbsHandlerCenter
+    {
+       HandlerInterface login;
+       
+       public HandlerCenter() {
+           login = new LoginHandler();
+       }
+
+        public override void ClientClose(UserToken token, string error)
+        {
+            Console.WriteLine("有客户端断开连接了");
+            login.ClientClose(token, error);
+        }
+
+        public override void ClientConnect(UserToken token)
+        {
+            Console.WriteLine("有客户端连接了");
+        }
+
+        public override void MessageReceive(UserToken token, object message)
+        {
+            SocketModel model = message as SocketModel;
+            switch (model.type) { 
+                case Protocol.TYPE_LOGIN:
+                    login.MessageReceive(token, model);
+                    break;
+            }
+        }
+    }
+}
